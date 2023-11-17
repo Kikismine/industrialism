c5a0b21c161934593db9e2010e4effb498f82633
Kikismine <krystelik353@gmail.com>
2023-11-17 22:19:09 +0100

    shaders

--- res/shaders/fragment.fs
+++
@@ -1,6 +0,0 @@
-#version 330 core
-out vec4 FragColor;
-
-void main() {
-    FragColor = vec4(0.33, 0.71, 0.21, 1.0);
-}
--- src/gfx/renderer.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef RENDERER_HPP
-#define RENDERER_HPP
-
-#include "../util/util.hpp"
-
-void render_init(GLFWwindow *window);
-void render(GLFWwindow *window);
-
-#endif /* ifndef RENDERER_HPP */
